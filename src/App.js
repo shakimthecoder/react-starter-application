b951d43738548245593ee867bb071170982fb303
amenraprod <amenraprod@gmail.com>
2019-08-14 14:29:12 -0400

    added label var

--- src/App.js
+++ src/App.js
@@ -6,6 +6,19 @@ const App = () => {
   const buttonText = { text: 'Click me'};
   const labelText = 'Enter name:';
 
-}
+  return(
+
+  <div>
+
+   <label className="label" htmlFor="name">
+    { labelText }
+   </label>
+
+
+</div>
+
+
+  );
+};
 
 export default App;
