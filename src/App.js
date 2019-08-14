8d34525c010f36d67ec8f6bc7df48c76206c7e63
amenraprod <amenraprod@gmail.com>
2019-08-14 14:26:15 -0400

    added btn and label

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import './App.css';
+import ReactDOM from 'react-dom';
 
+const App = () => {
+  const buttonText = { text: 'Click me'};
+  const labelText = 'Enter name:';
+
+}
 
 export default App;
