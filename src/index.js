ce490cddaf4dfc7a7d6b32796f2e68100ce09509
amenraprod <amenraprod@gmail.com>
2019-08-14 19:03:50 -0400

    added time ago prop

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ import CommentDetail from './CommentDetail';
 const App = () => {
     return (
       <div className="ui container comments">
-       <CommentDetail author="sam" />
-       <CommentDetail author="bob"/>
-       <CommentDetail author="james" />
-       <CommentDetail author="katrina"/>
+       <CommentDetail author="sam" timeAgo="Today at 5:55PM" />
+       <CommentDetail author="bob" timeAgo="Today at 3:33PM"/>
+       <CommentDetail author="james" timeAgo="Today at 2:22PM"/>
+       <CommentDetail author="katrina" timeAgo="Today at 1:00am"/>
 
     </div>
     );
