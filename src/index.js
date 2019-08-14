5032db2ecbd8066936688206abbeb7dafc2f96dc
amenraprod <amenraprod@gmail.com>
2019-08-14 19:06:32 -0400

    added text props

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ import CommentDetail from './CommentDetail';
 const App = () => {
     return (
       <div className="ui container comments">
-       <CommentDetail author="sam" timeAgo="Today at 5:55PM" />
-       <CommentDetail author="bob" timeAgo="Today at 3:33PM"/>
-       <CommentDetail author="james" timeAgo="Today at 2:22PM"/>
-       <CommentDetail author="katrina" timeAgo="Today at 1:00am"/>
+       <CommentDetail author="sam" timeAgo="Today at 5:55PM" text="Welcome" />
+       <CommentDetail author="bob" timeAgo="Today at 3:33PM" text="Life is good" />
+       <CommentDetail author="james" timeAgo="Today at 2:22PM" text="happy" />
+       <CommentDetail author="katrina" timeAgo="Today at 1:00am" text="no need" />
 
     </div>
     );
