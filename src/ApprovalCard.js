713bc16e7b130a1abc4e9586f08a1e17d244024d
amenraprod <amenraprod@gmail.com>
2019-08-14 19:50:44 -0400

    added sk

--- src/ApprovalCard.js
+++ src/ApprovalCard.js
@@ -2,6 +2,18 @@ import react from 'react';
 
 const ApprovalCard = () => {
 
+    return (
+     <div className="ui card">
+      <div className="ui content">
+       Text
+       </div>
+        <div className="additional content">
+         <div className="buttons">
+          </div>
+         </div>
+      </div>
+
+    );
 };
 
 export default ApprovalCard;
